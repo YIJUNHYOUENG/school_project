d5ec20b0f89cf985029e40dffac417168b6ba6d8
junhyeong <ljh040325@gmail.com>
2022-09-11 23:27:17 +0900

    오류 해결 실패

--- src/_reducers/user_reducers.js
+++ src/_reducers/user_reducers.js
@@ -6,6 +6,10 @@ export default function(state, action) {
     switch (action.type) {
         case LOGIN_USER:
             return { ...state, loginSuccess: action.payload }
+            break;
+
+        default:
+            return state;
 
     }
 }
