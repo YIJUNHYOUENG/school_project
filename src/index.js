5a0518f54bc9701ce8557b98eac051e4fe9a4e9a
junhyeong <ljh040325@gmail.com>
2022-09-12 23:51:28 +0900

    오늘도 오류 찾기 실패

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import * as ServiceWorker from './serviceWorker'
+// import * as ServiceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import ReduxThunk from "redux-thunk";
