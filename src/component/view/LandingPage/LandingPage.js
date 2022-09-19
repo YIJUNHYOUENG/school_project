41efc83fa0fb8e177cb1b73c3597c6d0f9cf6e48
junhyeong <ljh040325@gmail.com>
2022-09-19 23:47:44 +0900

    로그인/로그아웃 변경

--- src/component/view/LandingPage/LandingPage.js
+++ src/component/view/LandingPage/LandingPage.js
@@ -1,8 +1,17 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
+import './LandingPage.css';
 
-function LandingPage() {
+function LandingPage() { 
+
+  // function constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     isLogin: false,
+  //     accessToken: "",
+  //   };
+  // }
 
   const navigate = useNavigate();
 
@@ -17,21 +26,37 @@ function LandingPage() {
     })
   }
 
-  return (
-    <div>
-      <header>
-        <div>
-          {/* logo */}
-        </div>
+  const LoginCheck = () => {
+    let check = "로그인";
+    axios.get('/login')
+    .then(response => {
+      if(response.payload.loginSuccess) {
+        console.log(true)
+        check = "로그아웃";
+      }
+    })
+    return check;
+  }
 
-        <nav>
-          <ul>
-            <li><Link to="/register">회원가입</Link></li>
-            <li><Link to="/login">로그인</Link></li>
-          </ul>
-        </nav>
+  return (
+    <div className='layout'>
+      {/* header */}
+      <header className='header'>
+        <div className='contents'>        
+          <div>
+            {/* logo */}
+          </div>
+
+          <nav className='navigation'>
+            <ul>
+              <li><Link to="/register">회원가입</Link></li>
+              <li><Link to="/login">{LoginCheck()}</Link></li>
+            </ul>
+          </nav>
+        </div>
       </header>
 
+      {/* body */}
       <button onClick={onClickHandler}>
         로그아웃
       </button>
